5284232483354b68bcc5523db22202e519ffe9ef
Tommy Morgan <tommy@tommymorgan.com>
2022-09-06 09:47:26 -0500

    WIP

--- src/git-operations.ts
+++ src/git-operations.ts
@@ -1,5 +1,5 @@
-import { exec } from "@actions/exec";
+import { exec } from '@actions/exec';
 
 const git = async (args?: string[]) => {
-    return await exec('git', args);
+  return await exec('git', args);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,96 @@
-import * as core from "@actions/core";
-import * as exec from "@actions/exec";
-import * as github from "@actions/github";
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
 
-// steps:
-// - uses: actions/checkout@v3
-// - uses: actions/setup-node@v1
-//   with:
-//     always-auth: true
-//     node-version: 16.x
-//     registry-url: https://npm.pkg.github.com
-//     scope: ${{ inputs.scope }}
-// - id: publish
-//   # this will only run if the PR has been merged and will not run if the PR was closed without being merged
-//   if: github.event.pull_request.merged == true
-//   run: ${{ github.action_path }}/entrypoint.sh
-//   shell: bash
-//   env:
-//     NODE_AUTH_TOKEN: ${{ inputs.auth-token }}
-//     PREFIX: ${{ inputs.version-prefix }}
-// - id: release
-//   # this will only run if the PR has been merged and will not run if the PR was closed without being merged
-//   if: github.event.pull_request.merged == true
-//   uses: ncipollo/release-action@v1
-//   with:
-//     generateReleaseNotes: true
-//     tag: ${{ steps.publish.outputs.version }}
+const getClient = () => {
+  const token = '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'; // TODO: replace this with the appropriate environment variable
+  return github.getOctokit(token);
+};
+
+const getVersion = async (ref?: string): Promise<string> => {
+  const octokit = getClient();
+  const owner = 'icanbwell'; // TODO: replace this with the appropriate environment variable
+  const repo = 'tommy-mfe-release'; // TODO: replace this with the appropriate environment variable
+
+  const packageJson = await octokit.rest.repos.getContent({
+    headers: {
+      accept: 'application/vnd.github.VERSION.raw'
+    },
+    owner,
+    path: 'package.json',
+    repo,
+    ref
+  });
+
+  return '' + JSON.parse(packageJson.data.toString())?.version;
+};
+
+const queryTag = async (tag: string) => {
+  const owner = 'icanbwell'; // TODO: replace this with the appropriate environment variable
+  const repo = 'tommy-mfe-release'; // TODO: replace this with the appropriate environment variable
+  const octokit = getClient();
+
+  const result = await octokit.rest.git.getRef({
+    owner,
+    ref: `tags/${tag}`,
+    repo
+  });
+  return result.status === 200 ? tag : undefined;
+};
+
+/**
+ * Determines the next available semver version number.  Compares the version from the target branch
+ * with the proposed new version to infer the change type (MAJOR, MINOR, PATCH).  If the two versions
+ * differ in more than one "type" category then an exception is thrown.
+ * @param oldVersion The version in the target branch.
+ * @param newVersion The version the developer is proposing.
+ * @throws Will throw an exception if the two version numbers represent incompatible change types.
+ */
+const calculateNextVersion = (previousVersion: string, newVersion: string) => {
+  const newPieces = newVersion.split('.');
+  const previousPieces = previousVersion.split('.');
+  let incremented = false;
+
+  const incrementer = (a: number, b: number) => {
+    if (a === b) {
+      return a;
+    }
+
+    if (!incremented) {
+      incremented = true;
+      return Math.max(a, b) + 1;
+    }
+
+    throw new Error('Incompatible change types detected.');
+  };
+
+  const major = incrementer(+previousPieces[0], +newPieces[0]);
+  const minor = incrementer(+previousPieces[1], +newPieces[1]);
+  const patch = incrementer(+previousPieces[2], +newPieces[2]);
+
+  return `${major}.${minor}.${patch}`;
+};
 
 export async function run() {
-  const prefix = core.getInput("version-prefix");
-//   const token = core.getInput('github-token');
-//   const octokit = github.getOctokit(token);
-//   octokit.
-
-//   github.getOctokit
+  const prefix = core.getInput('version-prefix') || 'v';
+  //   const repo = core.getInput("repository"); // TODO: replace this with the appropriate environment variable
+  //   const token = core.getInput("github-token"); // TODO: replace this with the appropriate environment variable
+  const octokit = getClient();
 
   // get the new version number
-  const {stdout: ls} = await exec.getExecOutput(
-    'ls',
-    ['-la'],
-    {ignoreReturnCode: true, silent: true}
-  );
-  console.log('ls', ls);
+  let newVersion = await getVersion('TECHX-540');
 
-  const {stdout: pwd} = await exec.getExecOutput(
-    'pwd',
-    [],
-    {ignoreReturnCode: true, silent: true}
-  );
-  console.log('pwd', pwd);
   // determine if the new version number is available
+  const tag = await queryTag(`${prefix}${newVersion}`);
 
   // if it is not available
-  // get the previous version number
-  // determine whether this was a MAJOR, MINOR, or PATCH bump
-  // find the next available version for the given change type
+  if (tag) {
+    // get the previous version number
+    const previousVersion = await getVersion('main');
+    newVersion = calculateNextVersion(previousVersion, newVersion);
+  }
+
+  console.log(`The new version will be ${newVersion}`);
 
   // publish
   // release
